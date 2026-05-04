f24571836e765c9cfb93964a2bab76032f9f4d1f
pxnan <prasastimuslim@gmail.com>
2026-05-04 17:49:41 +0900

    Update CORS configuration to allow credentials in responses

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def add_cors_headers(response):
     
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-API-Key'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Credentials'] = 'false'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response
 
 # Tangani preflight OPTIONS request secara global
